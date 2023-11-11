9e992a127fc1569886c3f5890d5efcfe639af3f8
yes-ee <yes0112@naver.com>
2023-11-12 01:21:02 +0900

    feat(inputView): 날짜, 메뉴 입력 함수 생성

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,5 +1,15 @@
 package christmas.view;
 
-public class InputView {
+import christmas.constant.ServiceMessage;
 
+public class InputView {
+    public String inputDate() {
+        System.out.println(ServiceMessage.INPUT_DATE.getMessage());
+        return System.console().readLine();
+    }
+
+    public String inputMenu() {
+        System.out.println(ServiceMessage.INPUT_MENU.getMessage());
+        return System.console().readLine();
+    }
 }
