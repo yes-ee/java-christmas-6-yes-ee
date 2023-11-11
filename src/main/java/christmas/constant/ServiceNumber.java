18efdd67031bd5e75edb1b75fde7f9ff03eea15e
yes-ee <yes0112@naver.com>
2023-11-12 01:15:05 +0900

    feat(serviceNumber): enum ServiceNumber 클래스 생성

---
+++ src/main/java/christmas/constant/ServiceNumber.java
@@ -0,0 +1,24 @@
+package christmas.constant;
+
+public enum ServiceNumber {
+    DATE_MIN(1),
+    DATE_MAX(31),
+    MENU_MIN(1),
+    MENU_MAX(20),
+    EVENT_MIN_PRICE(10000),
+    DDAY_DISCOUNT_END(25),
+    WEEKDAY_DISCOUNT(2023),
+    WEEKEND_DISCOUNT(2023),
+    SPECIAL_DISCOUNT(1000),
+    FREE_EVENT_MIN_PRICE(120000);
+
+    private int number;
+
+    ServiceNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
