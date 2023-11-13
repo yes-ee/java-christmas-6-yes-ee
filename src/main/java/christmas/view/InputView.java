6cdaf7eb28c7a2c829e05ffef03618e50a8101da
yes-ee <yes0112@naver.com>
2023-11-13 20:24:52 +0900

    feat(inputView): 날짜 입력 함수 생성
    날짜 입력 요청 메시지 출력 후 입력받음

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,5 +1,12 @@
 package christmas.view;
 
+import christmas.constant.ServiceMessage;
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public static String inputDate() {
+        System.out.println(ServiceMessage.INPUT_DATE.getMessage());
+        return Console.readLine();
+    }
 
 }
