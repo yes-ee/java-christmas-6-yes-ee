ed9e6a0e965436d235b381b4acb2647518dde31d
yes-ee <yes0112@naver.com>
2023-11-13 20:29:17 +0900

    feat(outputView): 에러 메시지 출력 함수 생성
    [ERROR] + 에러 메시지 형식으로 출력

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,10 @@
 package christmas.view;
 
+import christmas.constant.ErrorMessage;
+
 public class OutputView {
+    public static void printErrorMessage(String message) {
+        System.out.println(ErrorMessage.ERROR_PREFIX.getMessage() + message);
+    }
 
 }
