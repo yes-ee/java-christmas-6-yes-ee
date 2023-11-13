0f3703bd1b4cb45b515e45359e1e1beac149e1b8
yes-ee <yes0112@naver.com>
2023-11-14 02:51:40 +0900

    test(outputViewTest): 에러 메시지 출력 테스트 추가

--- src/test/java/christmas/OutputViewTest.java
+++ src/test/java/christmas/OutputViewTest.java
@@ -2,6 +2,7 @@ package christmas;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import christmas.constant.ErrorMessage;
 import christmas.domain.Menu;
 import christmas.view.OutputView;
 import java.io.ByteArrayOutputStream;
@@ -13,6 +14,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("출력 테스트")
 public class OutputViewTest {
     private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
     private final PrintStream standardOut = System.out;
@@ -25,6 +27,20 @@ public class OutputViewTest {
     @AfterEach
     void tearDown() {
         System.setOut(standardOut);
+    }
+
+    @DisplayName("에러 메시지 출력 기능")
+    @Test
+    void printError() {
+        // given
+        String message = ErrorMessage.DATE_WRONG.getMessage();
+
+        // when
+        OutputView.printErrorMessage(message);
+
+        // then
+        assertThat(outputStream.toString())
+                .contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     }
 
     @DisplayName("주문 내역 출력 기능")
