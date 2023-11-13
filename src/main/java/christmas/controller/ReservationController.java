283cde5ff66d792d2cf6bdd93dba81206872bfb1
yes-ee <yes0112@naver.com>
2023-11-13 17:36:04 +0900

    feat(reservationController): 메인 로직 작성
    ReservationController 4개 로직으로 구분
    - 날짜 선택
    - 메뉴 선택
    - 할인 적용
    - 배지 증정

--- src/main/java/christmas/controller/ReservationController.java
+++ src/main/java/christmas/controller/ReservationController.java
@@ -1,5 +1,39 @@
 package christmas.controller;
 
-public class ReservationController {
+import christmas.service.BadgeService;
+import christmas.service.DateService;
+import christmas.service.DiscountService;
+import christmas.service.MenuService;
 
+public class ReservationController {
+    private DateService dateService;
+    private MenuService menuService;
+    private DiscountService discountService;
+    private BadgeService badgeService;
+
+    ReservationController() {
+        dateService = new DateService();
+        menuService = new MenuService();
+        discountService = new DiscountService();
+        badgeService = new BadgeService();
+    }
+
+    public void run() {
+        selectDate();
+        selectMenu();
+        applyDiscount();
+        giveBadge();
+    }
+
+    private void selectDate() {
+    }
+
+    private void selectMenu() {
+    }
+
+    private void applyDiscount() {
+    }
+
+    private void giveBadge() {
+    }
 }
