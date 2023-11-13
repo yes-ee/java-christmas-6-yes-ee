91b7f0b9335d6f3afceb0deae1a0f759eb8b9f13
yes-ee <yes0112@naver.com>
2023-11-14 01:24:51 +0900

    feat(menuService): 메뉴 입력 기능 생성
    메뉴 입력 받음
    메뉴 입력 형식 잘못된 경우('메뉴이름-개수' 형식 아닌 경우) 에러 처리

--- src/main/java/christmas/service/MenuService.java
+++ src/main/java/christmas/service/MenuService.java
@@ -1,5 +1,42 @@
 package christmas.service;
 
-public class MenuService {
+import christmas.constant.ErrorMessage;
+import christmas.domain.Menu;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+import java.util.Map;
 
+public class MenuService {
+    private Map<Menu, Integer> orderList;
+
+    public void chooseMenu() {
+        boolean isMenuValid = false;
+
+        while (!isMenuValid) {
+            try {
+                orderList.clear();
+
+                String [] orders = InputView.inputMenu().split(",");
+
+                makeOrderList(orders);
+
+                isMenuValid = true;
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(ErrorMessage.MENU_WRONG.getMessage());
+            }
+        }
+    }
+
+    private void makeOrderList(String[] orders) {
+        for (String order: orders) {
+            String [] splitOrder = order.split("-");
+
+            checkSplitOrder(splitOrder);
+        }
+    }
+
+    private void checkSplitOrder(String[] splitOrder) {
+        if (splitOrder.length != 2)
+            throw new IllegalArgumentException();
+    }
 }
