52a8ffe88843a360d40fb1e95e60811d59120524
yes-ee <yes0112@naver.com>
2023-11-14 02:14:38 +0900

    test(menuServiceTest): 메뉴판에 없는 메뉴 입력한 경우 예외 테스트 추가

--- src/test/java/christmas/MenuServiceTest.java
+++ src/test/java/christmas/MenuServiceTest.java
@@ -2,7 +2,10 @@ package christmas;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import christmas.domain.Menu;
 import christmas.service.MenuService;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,12 +17,25 @@ public class MenuServiceTest {
     @DisplayName("메뉴 입력 형식 잘못된 경우")
     @Test
     void menuFormatNotValid() {
-        //given
+        // given
         String [] splitOrder = {"해산물파스타1"};
 
         // then
         assertThatThrownBy(
                 () -> Validation.validateSplitOrder(splitOrder))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("주문한 메뉴 존재하지 않는 경우")
+    @Test
+    void menuNotExist() {
+        // given
+        Menu orderMenu = null;
+        Map<Menu, Integer> orderList = new HashMap<>();
+
+        // then
+        assertThatThrownBy(
+                () -> Validation.validateMenu(orderMenu, orderList))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
