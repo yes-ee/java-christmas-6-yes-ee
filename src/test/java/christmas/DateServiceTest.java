e1333e2c355ae2826a2a2c32259a24027f97a3ef
yes-ee <yes0112@naver.com>
2023-11-14 00:13:33 +0900

    test(dataServiceTest): 날짜 숫자 아닌 경우 예외 테스트 추가

--- src/test/java/christmas/DateServiceTest.java
+++ src/test/java/christmas/DateServiceTest.java
@@ -8,10 +8,20 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class DateServiceTest {
 
+    @DisplayName("날짜에 숫자 아닌 값 들어오는 경우 예외 테스트")
+    @ParameterizedTest
+    @ValueSource(strings={"a", "b2", "11c"})
+    void dateNotNumberTest(String input) {
+        // then
+        assertThatThrownBy(
+                () -> Validation.validateDate(Integer.parseInt(input)))
+                .isInstanceOf(NumberFormatException.class);
+    }
+
     @DisplayName("날짜 범위 예외 테스트")
     @ParameterizedTest
     @ValueSource(ints={0, 32, 100, -1})
-    void dateValidationTest(int input) {
+    void dateRangeTest(int input) {
         // then
         assertThatThrownBy(
                 () -> Validation.validateDate(input))
