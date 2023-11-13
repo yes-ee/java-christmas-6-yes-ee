da339d8801a2dd774f6fbed9f8c3d0d2f4f62f3d
yes-ee <yes0112@naver.com>
2023-11-14 02:24:08 +0900

    docs(readme): 기능 목록 완료 체크

--- src/test/java/christmas/MenuServiceTest.java
+++ src/test/java/christmas/MenuServiceTest.java
@@ -3,13 +3,10 @@ package christmas;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import christmas.domain.Menu;
-import christmas.service.MenuService;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("메뉴 서비스 테스트")
 public class MenuServiceTest {
