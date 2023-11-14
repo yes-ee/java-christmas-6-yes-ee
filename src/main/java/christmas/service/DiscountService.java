f9d6468381fe3c460a8010438ebf8d044eef8f15
yes-ee <yes0112@naver.com>
2023-11-14 18:55:59 +0900

    feat(discountService): 크리스마스 디데이 할인 기능 추가

--- src/main/java/christmas/service/DiscountService.java
+++ src/main/java/christmas/service/DiscountService.java
@@ -1,12 +1,16 @@
 package christmas.service;
 
+import christmas.constant.ServiceNumber;
+import christmas.domain.Discount;
 import christmas.domain.Menu;
+import java.util.HashMap;
 import java.util.Map;
 
 public class DiscountService {
     int date;
     Map<Menu, Integer> orderList;
     int orderPrice;
+    Map<Discount, Integer> discountList = new HashMap<>();
     int benefitPrice = 0;
 
     public DiscountService(int date, Map<Menu, Integer> orderList, int orderPrice) {
@@ -16,9 +20,22 @@ public class DiscountService {
     }
 
     public void applyDiscount() {
+        applyDdayDiscount();
     }
 
     public int getBenefitPrice() {
         return benefitPrice;
+    }
+
+    private void applyDdayDiscount() {
+        if (date > ServiceNumber.DDAY_DISCOUNT_END.getNumber()) {
+            return;
+        }
+
+        discountList.put(Discount.DDAY_DISCOUNT, getDdayBenefitPrice());
+    }
+
+    private int getDdayBenefitPrice() {
+        return Discount.DDAY_DISCOUNT.getPrice() + (date - ServiceNumber.DATE_MIN.getNumber()) * 100;
     }
 }
