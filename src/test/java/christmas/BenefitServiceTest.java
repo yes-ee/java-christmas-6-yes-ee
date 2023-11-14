93d2b503f93ae12b4bd5eb5454f2751d80250e8f
yes-ee <yes0112@naver.com>
2023-11-14 23:19:32 +0900

    test(benefitServiceTest): 평일 할인 테스트 추가

--- src/test/java/christmas/BenefitServiceTest.java
+++ src/test/java/christmas/BenefitServiceTest.java
@@ -1,7 +1,12 @@
 package christmas;
 
+import christmas.domain.Menu;
 import christmas.service.DdayDiscountService;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.service.WeekdayDiscountService;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -18,6 +23,25 @@ public class BenefitServiceTest {
 
         // when
         discountService.applyDiscount(date);
+        int discountPrice = discountService.getBenefitPrice();
+
+        // then
+        assertThat(discountPrice).isEqualTo(expected);
+    }
+
+    @DisplayName("평일 할인 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1:0", "2:0", "8:0", "5:4046", "11: 4046"}, delimiter = ':')
+    void weekdayDiscount(int date, int expected) {
+        // given
+        WeekdayDiscountService discountService = new WeekdayDiscountService();
+        Map<Menu, Integer> orderList = new HashMap<>();
+        orderList.put(Menu.CHOCOLATE_CAKE, 1);
+        orderList.put(Menu.ICE_CREAM, 1);
+        orderList.put(Menu.SEAFOOD_PASTA, 1);
+
+        // when
+        discountService.applyDiscount(date, orderList);
         int discountPrice = discountService.getBenefitPrice();
 
         // then
