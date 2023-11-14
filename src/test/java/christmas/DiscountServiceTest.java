f9294776b8c75bc01e9e91f5535a1d41b2a3904b
yes-ee <yes0112@naver.com>
2023-11-14 22:29:23 +0900

    test(discountServiceTest): 디데이 할인 테스트 추가

---
+++ src/test/java/christmas/DiscountServiceTest.java
@@ -0,0 +1,25 @@
+package christmas;
+
+import christmas.service.DdayDiscountService;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@DisplayName("할인 서비스 테스트")
+public class DiscountServiceTest {
+
+    @DisplayName("크리스마스 디데이 할인 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1:1000", "25:3400", "10:1900", "26:0"}, delimiter = ':')
+    void ddayDiscount(int date, int expected) {
+        // given
+        DdayDiscountService discountService = new DdayDiscountService();
+
+        // when
+        int discountPrice = discountService.applyDiscount(date);
+
+        // then
+        assertThat(discountPrice).isEqualTo(expected);
+    }
+}
