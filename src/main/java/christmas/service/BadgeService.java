e52c750a1fc842051eda3e88001ef568753f2303
yes-ee <yes0112@naver.com>
2023-11-15 13:40:36 +0900

    feat(badgeService): 이벤트 배지 적용 기능 추가

--- src/main/java/christmas/service/BadgeService.java
+++ src/main/java/christmas/service/BadgeService.java
@@ -1,13 +1,27 @@
 package christmas.service;
 
 
+import christmas.domain.Badge;
+
 public class BadgeService {
     int benefitPrice;
+    Badge badge;
 
     public BadgeService(int benefitPrice) {
         this.benefitPrice = benefitPrice;
     }
 
     public void applyBadge() {
+        if (benefitPrice >= Badge.SANTA.getBenefitPrice()) {
+            badge = Badge.SANTA;
+        } else if (benefitPrice >= Badge.TREE.getBenefitPrice()) {
+            badge = Badge.TREE;
+        } else if (benefitPrice >= Badge.STAR.getBenefitPrice()) {
+            badge = Badge.STAR;
+        }
+    }
+
+    public Badge getBadge() {
+        return badge;
     }
 }
