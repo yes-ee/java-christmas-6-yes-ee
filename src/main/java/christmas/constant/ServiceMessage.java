7d10852855feb5c15a0bd06b5c72772141a812c8
yes-ee <yes0112@naver.com>
2023-11-15 14:26:45 +0900

    refactor(serviceMessage): 증정 메뉴 이름 변경

--- src/main/java/christmas/constant/ServiceMessage.java
+++ src/main/java/christmas/constant/ServiceMessage.java
@@ -7,7 +7,7 @@ public enum ServiceMessage {
     OUTPUT_BENEFIT_PREVIEW("일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
     OUTPUT_MENU("<주문 메뉴>"),
     OUTPUT_BEFORE_DISCOUNT("<할인 전 총주문 금액>"),
-    OUTPUT_FREE_MENU("<증정 메뉴>"),
+    OUTPUT_GIVEAWAY_MENU("<증정 메뉴>"),
     OUTPUT_BENEFIT_HISTORY("<혜택 내역>"),
     OUTPUT_BENEFIT_PRICE("<총혜택 금액>"),
     OUTPUT_AFTER_DISCOUNT("<할인 후 예상 결제 금액>"),
