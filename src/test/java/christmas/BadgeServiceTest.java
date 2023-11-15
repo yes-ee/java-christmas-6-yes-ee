af3d42c48af04fefde18255ac1451426e04d195e
yes-ee <yes0112@naver.com>
2023-11-15 13:41:38 +0900

    test(badgeServiceTest): 금액에 따라 올바른 뱃지가 부여되는지 테스트

---
+++ src/test/java/christmas/BadgeServiceTest.java
@@ -0,0 +1,26 @@
+package christmas;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.service.BadgeService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@DisplayName("뱃지 서비스 테스트")
+public class BadgeServiceTest {
+    @DisplayName("구입 금액에 따라 뱃지가 부여되는지 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"5000:별", "15000:트리", "30000:산타"}, delimiter = ':')
+    void giveBadge(int price, String expected) {
+        // given
+        BadgeService badgeService = new BadgeService(price);
+
+        // when
+        badgeService.applyBadge();
+
+        // then
+        assertThat(badgeService.getBadge().getName()).isEqualTo(expected);
+    }
+
+}
